0563abade25f0d843642917bee9eb8c50787a382
Owen013 <96493201+Owen013@users.noreply.github.com>
2021-12-23 19:32:36 -0500

    Add files via upload

--- Owen013.TeenyHatchling/TeenyHatchling.cs
+++ Owen013.TeenyHatchling/TeenyHatchling.cs
@@ -20,17 +20,19 @@ namespace TeenyHatchling
             // Example of accessing game code.
             LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
             {
-                if (loadScene != OWScene.SolarSystem) return;
+                if (loadScene != OWScene.SolarSystem && loadScene != OWScene.EyeOfTheUniverse) return;
                 var playerBody = FindObjectOfType<PlayerBody>();
                 var playerModel = playerBody.transform.Find("Traveller_HEA_Player_v2");
                 var playerCamera = FindObjectOfType<PlayerCameraController>();
                 var stickRoot = playerBody.transform.Find("RoastingSystem").transform.Find("Stick_Root");
-                var cockpitController = FindObjectOfType<ShipCockpitController>();
-                var logController = FindObjectOfType<ShipLogController>();
                 playerModel.transform.localScale = new UnityEngine.Vector3(0.1f, 0.05f, 0.1f);
                 playerCamera._origLocalPosition = new UnityEngine.Vector3(0f, 0.1f, 0.15f);
                 stickRoot.transform.localPosition = new UnityEngine.Vector3(0.25f, -0.75f, 0.08f);
                 stickRoot.transform.localRotation = new UnityEngine.Quaternion(-0.0868f, -0.0868f, -0.0076f, 0.9924f);
+                ModHelper.Console.WriteLine($"All done!", MessageType.Success);
+                if (loadScene != OWScene.SolarSystem) return;
+                var cockpitController = FindObjectOfType<ShipCockpitController>();
+                var logController = FindObjectOfType<ShipLogController>();
                 ModHelper.Events.Unity.FireInNUpdates(() => cockpitController._origAttachPointLocalPos = new UnityEngine.Vector3(0f, 1.1f, 4.2307f), 60);
                 logController._attachPoint._attachOffset = new UnityEngine.Vector3(0f, 0.75f, 0f);
                 ModHelper.Console.WriteLine($"All done!", MessageType.Success);
